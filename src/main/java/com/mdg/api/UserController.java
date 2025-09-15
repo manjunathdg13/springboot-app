b9c24097697929d56558b83b82d2a32fb4122d2f
Manjunath Goudappanavar <dgmanjunath0@gmail.com>
2025-09-16 01:59:51 +0530

    added cors

--- src/main/java/com/mdg/api/UserController.java
+++ src/main/java/com/mdg/api/UserController.java
@@ -2,12 +2,13 @@ package com.mdg.api;
 
 import com.mdg.userService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@CrossOrigin(origins = "https://react-deploy-kor7.vercel.app")
 public class UserController {
     @Autowired
     UserService service;
