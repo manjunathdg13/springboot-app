1e7f5bf0a4b29c42a00db8f262af825fbe8d5e6c
Manjunath Goudappanavar <dgmanjunath0@gmail.com>
2025-09-16 02:19:51 +0530

    added todo

--- src/main/java/com/mdg/api/UserController.java
+++ src/main/java/com/mdg/api/UserController.java
@@ -1,5 +1,6 @@
 package com.mdg.api;
 
+import com.mdg.dto.Todo;
 import com.mdg.userService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,9 +8,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "https://react-deploy-kor7.vercel.app")
 public class UserController {
+    private final List<Todo> todos = new ArrayList<>();
+
+    public UserController() {
+        for (int i = 1; i <= 20; i++) {
+            todos.add(new Todo(i, "Todo task number " + i, i % 2 == 0));
+        }
+    }
+
+    // GET all todos
+    @GetMapping("/todos")
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    // GET single todo by id
+//
     @Autowired
     UserService service;
     @GetMapping("/{id}")
